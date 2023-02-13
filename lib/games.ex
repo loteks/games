51b4b7b0531f1e848a1f13b741ba1dd9108d9aff
Steve Freeman <steve@indieit.com>
2023-02-13 16:57:46 -0600

    Co-authored-by: Marko Vukovic <mvkvc.git@gmail.com>

--- lib/games.ex
+++ lib/games.ex
@@ -1,18 +1,2 @@
 defmodule Games do
-  @moduledoc """
-  Documentation for `Games`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Games.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
