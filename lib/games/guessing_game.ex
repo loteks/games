465eb7e69315073fef06f2f1e764aec342d29577
Steve Freeman <steve@indieit.com>
2023-02-13 16:58:04 -0600

    completed guessinggame and rockpaperscissors

---
+++ lib/games/guessing_game.ex
@@ -0,0 +1,28 @@
+defmodule Games.GuessingGame do
+  def play() do
+    value = Enum.random(1..10)
+
+    guess =
+      IO.gets("Enter your guess or q to quit: ")
+      |> String.trim()
+      IO.inspect(binding())
+
+      cond do
+        guess == "q" -> "Bye!"
+
+        value > String.to_integer(guess) ->
+          IO.puts("To low!")
+          play()
+
+          value < String.to_integer(guess) ->
+          IO.puts("To high!")
+          play()
+
+        value == String.to_integer(guess) -> "Correct!"
+
+        value != String.to_integer(guess) ->
+          IO.puts("Incorrect. The number was #{value}")
+          play()
+      end
+  end
+end
---
+++ lib/games/rock_paper_scissors.ex
@@ -0,0 +1,25 @@
+defmodule Games.RockPaperScissors do
+  def play() do
+    player2 = Enum.random([:rock, :paper, :scissors])
+    IO.puts(player2)
+
+    # worker bee
+    player1 =
+      IO.gets("Enter your guess: ")
+      |> String.trim()
+      |> String.to_existing_atom()
+      |> IO.inspect()
+
+
+      # functional core
+      case {player1, player2} do
+        {:rock, :scissors} -> "You win! Rock beats scissors."
+        {:paper, :rock} -> "You win! Paper beats rock."
+        {:scissors, :paper} -> "You win! Scissors beats paper."
+        {:rock, :paper} -> "You lose! Paper beats rock."
+        {:paper, :scissors} -> "You lose! Scissors beats paper."
+        {:scissors, :rock} -> "You lose! Rock beats scissors."
+        {same, same} -> "It's a tie!"
+      end
+  end
+end
